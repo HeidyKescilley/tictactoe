e1d85dc9a13879dda2b85601c972cc584263fa9a
Heidy Kescilley <kescilley@gmail.com>
2023-06-07 05:01:15 -0300

    projeto inicial

---
+++ index.js
@@ -0,0 +1,6 @@
+const vBoard = [[], [], []];
+
+document.getElementById("startBtn").addEventListener("click", function () {
+  const player1 = document.getElementById("player1").value;
+  const player2 = document.getElementById("player2").value;
+});
